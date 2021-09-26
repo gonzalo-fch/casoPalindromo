5cd2491fba44a56e345afcc13c31fb0f83298d6e
Gonzalo <g.caniupan01@ufromail.cl>
2021-09-26 13:19:50 -0400

    commit estudiante1 version 2.0

--- src/main/java/proyectoEnsayo.java
+++ src/main/java/proyectoEnsayo.java
@@ -25,4 +25,56 @@ public class proyectoEnsayo {
         } while (numero <= 0);
         return numero;
     }
+    public static int existeEnArreglo(String[] arreglo, String busqueda) {// arreglo string
+
+        /*
+        CODIGO QUE PERMITE EL FUNCIONAMIENTO
+
+
+        String[] palabras = {"palabra1", "palabra2", "palabra3"};
+        String pBuscada= "palabra1";
+        int posicionDelElemento = existeEnArreglo(palabras, pBuscada);
+        if (posicionDelElemento == -1) {
+            System.out.println("El elemento NO existe en el arreglo");
+
+        } else {
+            System.out.println("El elemento existe en la posición: " +posicionDelElemento);
+        }
+         */
+        for (int x = 0; x < arreglo.length; x++) {
+            if (arreglo[x].equals(busqueda)) {
+                return x; // si lo encuenta retorna la posicion en el arreglo
+            }
+        }
+        return -1;
+    }
+    public static int validar() {
+        Scanner teclado = new Scanner(System.in);
+        int numero1 = 0;
+
+        do {
+            try {
+                System.out.println("..........[]..........");
+                numero1 = teclado.nextInt();
+                if (numero1 < 0) {
+                    System.out.println("No se admiten numeros negativos");
+                } else if (numero1 > 100) {
+                    System.out.println("No se admiten numeros mayores a 100");
+                    numero1 = -1;
+
+                }
+            } catch (java.util.InputMismatchException e) {
+                System.out.println("Dato erroneo");
+                System.out.println("Intentelo denuevo");
+                teclado.next();
+
+            }
+
+        } while (numero1 <= 0);
+        return numero1;
+    }
+
+
+
+
 }
