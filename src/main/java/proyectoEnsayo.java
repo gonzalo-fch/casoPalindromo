7f486c251da9154ff1e05d9a13e35796d959fdf6
Gonzalo <g.caniupan01@ufromail.cl>
2021-09-26 13:22:41 -0400

    commit estudiante3 version 2.0

--- src/main/java/proyectoEnsayo.java
+++ src/main/java/proyectoEnsayo.java
@@ -58,5 +58,26 @@ public class proyectoEnsayo {
         return ultima;
 
     }
+    public static void mostrarMatriz(int[][] matriz) {
+        System.out.println("\n Su matriz seria la siguiente \n");
+        for (int[] ints : matriz) {
+            for (int j = 0; j < ints.length; j++) {
+                System.out.print(ints[j] + "\t ");
+
+            }
+            System.out.println();
+        }
+    }
+
+    public static int[][] llenarMatrizAleatorios(int[][] matriz) {
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                matriz[i][j] = (int) (Math.random() * 100) + 1;
+
+            }
+        }
+        return matriz;
+    }
+
 
 }
